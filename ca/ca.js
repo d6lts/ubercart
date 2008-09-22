7f52e8a28be338987c5b849bf95e8e6e22c74bd3
Lyle Mantooth <lyle@ubercart.org>
2008-09-22 21:07:48 +0000

    1213 Ryan	2008-09-18       Fixing trigger to use #title instead of #label on an argument.
     1214 Ryan	2008-09-18
          Support for adding and modifying actions on predicates.
    
     1215 Ryan	2008-09-19
          Added filtering of action/condition arguments by entity type and support for removing actions from predicates.
    
     1216 Ryan	2008-09-19
          Removing the old conditions form test.
    
     1217 Ryan	2008-09-19
          Moving actions form into a conditional inclue through the menu system.
    
     1218 Ryan	2008-09-19
          Make it so action fieldsets are collapsed instead of expanded by default w/ newly added actions always being expanded.
    
     1219 Ryan	2008-09-19
          Adding JS behavior to let users confirm when clicking remove buttons.
    
     1220 Ryan	2008-09-19
          Fixing bug with action removal leaving holes in the actions array keys.
    
     1221 Lyle	2008-09-22
          Port of Google Checkout integration.
    
     1222 Lyle	2008-09-22
          Upgrade path for UC 1.0 to UC 2.0. ID fields have been standardized and set to unsigned serial or int schema types.
    
     1223 Ryan	2008-09-22
          Conversion of most settings overviews to the new summaries API by cha0s (patch for country and product settings overviews pending); addition of CSS and JS by Ryan to enhance their display in Ubercart.

---
+++ ca/ca.js
@@ -0,0 +1,14 @@
+// $Id$
+
+/**
+ * @file
+ *   Adds some helper JS to the conditional actions forms.
+ */
+
+// Adds confirmation prompts to remove buttons.
+Drupal.behaviors.caRemoveConfirm = function(context) {
+  $('.ca-remove-confirm:not(.caRemoveConfirm-processed)', context).addClass('caRemoveConfirm-processed').click(function() {
+    return confirm(Drupal.t('Are you sure you want to remove this item?'));
+  });
+}
+
