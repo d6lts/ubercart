54efb7f8a250e3c52cdd783b04f03285ef490bd8
Dave Long <dave@longwaveconsulting.com>
2012-11-06 18:46:12 +0000

    Convert order entity and storage controller to PSR-0.

---
+++ uc_order/lib/Drupal/uc_order/Plugin/Core/Entity/UcOrder.php
@@ -0,0 +1,110 @@
+<?php
+
+/**
+ * @file
+ * Definition of Drupal\uc_order\Plugin\Core\Entity\UcOrder.
+ */
+
+namespace Drupal\uc_order\Plugin\Core\Entity;
+
+use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Entity\Entity;
+use Drupal\Core\Annotation\Plugin;
+use Drupal\Core\Annotation\Translation;
+
+/**
+ * Defines the order entity class.
+ *
+ * @Plugin(
+ *   id = "uc_order",
+ *   label = @Translation("Order"),
+ *   module = "uc_order",
+ *   controller_class = "Drupal\uc_order\UcOrderStorageController",
+ *   base_table = "uc_orders",
+ *   fieldable = TRUE,
+ *   entity_keys = {
+ *     "id" = "order_id",
+ *   },
+ *   view_modes = {
+ *     "admin" = {
+ *       "label" = "Admin view",
+ *       "custom_settings" = FALSE
+ *     },
+ *     "customer" = {
+ *       "label" = "Customer view",
+ *       "custom_settings" = TRUE
+ *     },
+ *   }
+ * )
+ */
+class UcOrder extends Entity implements ContentEntityInterface {
+
+  /**
+   * The order ID.
+   *
+   * @var integer
+   */
+  public $order_id;
+
+  /**
+   * The order owner's user ID.
+   *
+   * @var integer
+   */
+  public $uid;
+
+  public $currency = '';
+  public $order_status = '';
+  public $order_total = 0;
+  public $primary_email = '';
+
+  public $delivery_first_name = '';
+  public $delivery_last_name = '';
+  public $delivery_phone = '';
+  public $delivery_company = '';
+  public $delivery_street1 = '';
+  public $delivery_street2 = '';
+  public $delivery_city = '';
+  public $delivery_zone = 0;
+  public $delivery_postal_code = '';
+  public $delivery_country = 0;
+
+  public $billing_first_name = '';
+  public $billing_last_name = '';
+  public $billing_phone = '';
+  public $billing_company = '';
+  public $billing_street1 = '';
+  public $billing_street2 = '';
+  public $billing_city = '';
+  public $billing_zone = 0;
+  public $billing_postal_code = '';
+  public $billing_country = 0;
+
+  public $products = array();
+  public $line_items = array();
+
+  public $payment_method = '';
+  public $data = array();
+
+  /**
+   * The order creation timestamp.
+   *
+   * @var integer
+   */
+  public $created;
+
+  /**
+   * The order modification timestamp.
+   *
+   * @var integer
+   */
+  public $modified;
+
+  /**
+   * Implements Drupal\Core\Entity\EntityInterface::id().
+   */
+  public function id() {
+    return $this->order_id;
+  }
+
+}
---
+++ uc_order/lib/Drupal/uc_order/UcOrderStorageController.php
@@ -0,0 +1,179 @@
+<?php
+
+/**
+ * @file
+ * Definition of Drupal\uc_order\UcOrderStorageController.
+ */
+
+namespace Drupal\uc_order;
+
+use Drupal\Core\Entity\DatabaseStorageController;
+use Drupal\Core\Entity\EntityInterface;
+
+/**
+ * Controller class for orders.
+ */
+class UcOrderStorageController extends DatabaseStorageController {
+
+  /**
+   * Overrides Drupal\Core\Entity\DatabaseStorageController::create().
+   */
+  public function create(array $values) {
+    $order = parent::create($values);
+
+    // Set the primary email address.
+    if (!empty($order->uid)) {
+      if ($account = user_load($order->uid)) {
+        $this->primary_email = $account->mail;
+      }
+    }
+
+    // Set the default order status.
+    if (empty($order->order_status)) {
+      $order->order_status = uc_order_state_default('in_checkout');
+    }
+
+    // Set the default currency.
+    if (empty($order->currency)) {
+      $order->currency = variable_get('uc_currency_code', 'USD');
+    }
+
+    // Set the default country codes.
+    if (empty($order->billing_country)) {
+      $order->billing_country = variable_get('uc_store_country', 840);
+    }
+    if (empty($order->delivery_country)) {
+      $order->delivery_country = variable_get('uc_store_country', 840);
+    }
+
+    // Set the created time to now.
+    if (empty($order->created)) {
+      $order->created = REQUEST_TIME;
+    }
+
+    return $order;
+  }
+
+  /**
+   * Overrides Drupal\Core\Entity\DatabaseStorageController::attachLoad().
+   */
+  protected function attachLoad(&$orders, $load_revision = FALSE) {
+    foreach ($orders as &$order) {
+      $order->data = unserialize($order->data);
+      $result = entity_query('uc_order_product')
+        ->condition('order_id', $order->order_id)
+        ->orderBy('order_product_id', 'ASC')
+        ->execute();
+      if (!empty($result)) {
+        $order->products = entity_load_multiple('uc_order_product', array_keys($result), TRUE);
+        foreach ($order->products as $product) {
+          $product->order = $order;
+          $product->order_uid = $order->uid;
+        }
+      }
+      else {
+        $order->products = array();
+      }
+
+      uc_order_module_invoke('load', $order, NULL);
+
+      // Load line items... has to be last after everything has been loaded.
+      $order->line_items = uc_order_load_line_items($order);
+
+      $fields = array();
+
+      // Make sure the total still matches up...
+      if (($total = uc_order_get_total($order)) !== $order->order_total) {
+        $fields['order_total'] = $total;
+        $order->order_total = $total;
+      }
+
+      if (($count = uc_order_get_product_count($order)) !== $order->product_count) {
+        $fields['product_count'] = $count;
+        $order->product_count = $count;
+      }
+
+      if (count($fields)) {
+        $query = db_update('uc_orders')
+          ->fields($fields)
+          ->condition('order_id', $order->order_id)
+          ->execute();
+      }
+    }
+
+    parent::attachLoad($orders, $load_revision);
+  }
+
+  /**
+   * Overrides Drupal\Core\Entity\DatabaseStorageController::preSave().
+   */
+  protected function preSave(EntityInterface $order) {
+    $order->order_total = uc_order_get_total($order);
+    $order->product_count = uc_order_get_product_count($order);
+    if (is_null($order->delivery_country) || $order->delivery_country == 0) {
+      $order->delivery_country = config('uc_store.settings')->get('address.country');
+    }
+    if (is_null($order->billing_country) || $order->billing_country == 0) {
+      $order->billing_country = config('uc_store.settings')->get('address.country');
+    }
+    $order->host = ip_address();
+    $order->modified = REQUEST_TIME;
+
+    uc_order_module_invoke('presave', $order, NULL);
+  }
+
+  /**
+   * Overrides Drupal\Core\Entity\DatabaseStorageController::postSave().
+   */
+  protected function postSave(EntityInterface $order, $update) {
+    foreach ($order->products as $product) {
+      drupal_alter('uc_order_product', $product, $order);
+      uc_order_product_save($order->order_id, $product);
+    }
+
+    uc_order_module_invoke('save', $order, NULL);
+    $order->order_total = uc_order_get_total($order);
+  }
+
+  /**
+   * Overrides Drupal\Core\Entity\DatabaseStorageController::preDelete().
+   */
+  protected function preDelete($orders) {
+    foreach ($orders as $order_id => $order) {
+      uc_order_module_invoke('delete', $order, NULL);
+    }
+  }
+
+  /**
+   * Overrides Drupal\Core\Entity\DatabaseStorageController::postDelete().
+   */
+  protected function postDelete($orders) {
+    // Delete data from the appropriate Ubercart order tables.
+    $ids = array_keys($orders);
+    $result = entity_query('uc_order_product')
+      ->condition('order_id', $ids, 'IN')
+      ->execute();
+    if (!empty($result)) {
+      $product_ids = array_keys($result);
+      uc_order_product_delete_multiple($product_ids);
+    }
+    db_delete('uc_order_comments')
+      ->condition('order_id', $ids, 'IN')
+      ->execute();
+    db_delete('uc_order_admin_comments')
+      ->condition('order_id', $ids, 'IN')
+      ->execute();
+    db_delete('uc_order_log')
+      ->condition('order_id', $ids, 'IN')
+      ->execute();
+
+    foreach ($orders as $order_id => $order) {
+      // Delete line items for the order.
+      uc_order_delete_line_item($order_id, TRUE);
+
+      // Log the action in the database.
+      watchdog('uc_order', 'Order @order_id deleted by user @uid.', array('@order_id' => $order_id, '@uid' => $GLOBALS['user']->uid));
+    }
+  }
+
+}
