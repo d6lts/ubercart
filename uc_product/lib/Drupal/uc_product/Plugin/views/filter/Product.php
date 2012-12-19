6e546c363465193999b40c9279ef0388a3b1021a
Dave Long <dave@longwaveconsulting.com>
2012-12-19 08:09:24 +0000

    Upgrade Views integration: product filter.

---
+++ uc_product/lib/Drupal/uc_product/Plugin/views/filter/Product.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @file
+ * Definition of Drupal\uc_product\Plugin\views\filter\Product.
+ */
+
+namespace Drupal\uc_product\Plugin\views\filter;
+
+use Drupal\views\Plugin\views\filter\BooleanOperator;
+use Drupal\Core\Annotation\Plugin;
+
+/**
+ * Filter handler for "is a product".
+ *
+ * @ingroup views_filter_handlers
+ *
+ * @Plugin(
+ *   id = "uc_product",
+ *   module = "uc_product"
+ * )
+ */
+class Product extends BooleanOperator {
+
+  /**
+   * Overrides BooleanOperator::query().
+   */
+  function query() {
+    $types = uc_product_types();
+    $this->query->addField('node', 'type');
+    $this->query->addWhere($this->options['group'], 'node.type', $types, empty($this->value) ? 'NOT IN' : 'IN');
+  }
+
+}
