e8991da49522977c756b61ca7b3441a16259cb84
Dave Long <dave@longwaveconsulting.com>
2012-12-19 08:39:24 +0000

    Upgrade Views integration: add to cart and buy it now fields.

---
+++ uc_product/lib/Drupal/uc_product/Plugin/views/field/BuyItNow.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * @file
+ * Definition of Drupal\uc_product\Plugin\views\field\BuyItNow.
+ */
+
+namespace Drupal\uc_product\Plugin\views\field;
+
+use Drupal\views\Plugin\views\field\FieldPluginBase;
+use Drupal\Core\Annotation\Plugin;
+
+/**
+ * Field handler to provide payment method.
+ *
+ * @ingroup views_field_handlers
+ *
+ * @Plugin(
+ *   id = "uc_product_buy_it_now",
+ *   module = "uc_product"
+ * )
+ */
+class BuyItNow extends FieldPluginBase {
+
+  /**
+   * Overrides FieldPluginBase::render().
+   */
+  function render($values) {
+    $nid = $this->get_value($values);
+    $node = node_load($nid);
+    if (uc_product_is_product($node)) {
+      $form = drupal_get_form('uc_catalog_buy_it_now_form_' . $nid, $node);
+      return drupal_render($form);
+    }
+  }
+
+}
