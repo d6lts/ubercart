539c09ed782c4087c32eb5117e79022fbe22d9dc
Dave Long <dave@longwaveconsulting.com>
2012-12-27 16:45:58 +0000

    Upgrade Views integration: Allow customer full name field to link to user.

--- uc_order/lib/Drupal/uc_order/Plugin/views/field/FullName.php
+++ uc_order/lib/Drupal/uc_order/Plugin/views/field/FullName.php
@@ -8,7 +8,9 @@
 namespace Drupal\uc_order\Plugin\views\field;
 
 use Drupal\views\Plugin\views\field\FieldPluginBase;
+use Drupal\views\Plugin\views\display\DisplayPluginBase;
 use Drupal\Core\Annotation\Plugin;
+use Drupal\views\ViewExecutable;
 
 /**
  * Field handler to provide full names.
@@ -22,16 +24,38 @@ use Drupal\Core\Annotation\Plugin;
  */
 class FullName extends FieldPluginBase {
 
+  /**
+   * Override init function to provide generic option to link to user.
+   */
+  public function init(ViewExecutable $view, DisplayPluginBase $display, array &$options = NULL) {
+    parent::init($view, $display, $options);
+    if (!empty($this->options['link_to_user'])) {
+      $this->additional_fields['uid'] = array('table' => 'uc_orders', 'field' => 'uid');
+    }
+  }
+
+  /**
+   * Overrides FieldPluginBase::defineOptions().
+   */
   protected function defineOptions() {
     $options = parent::defineOptions();
-
+    $options['link_to_user'] = array('default' => FALSE, 'bool' => TRUE);
     $options['format'] = array('default' => 'first_last');
-
     return $options;
   }
 
+  /**
+   * Overrides FieldPluginBase::buildOptionsForm().
+   */
   public function buildOptionsForm(&$form, &$form_state) {
     parent::buildOptionsForm($form, $form_state);
+
+    $form['link_to_user'] = array(
+      '#title' => t('Link this field to its user'),
+      '#description' => t("Enable to override this field's links."),
+      '#type' => 'checkbox',
+      '#default_value' => $this->options['link_to_user'],
+    );
 
     $form['format'] =  array(
       '#type' => 'select',
@@ -45,17 +69,37 @@ class FullName extends FieldPluginBase {
     );
   }
 
+  /**
+   * Renders whatever the data is as a link to the order.
+   *
+   * Data should be made XSS safe prior to calling this function.
+   */
+  function render_link($data, $values) {
+    if (!empty($this->options['link_to_user']) && user_access('access user profiles')) {
+      $this->options['alter']['make_link'] = TRUE;
+      $this->options['alter']['path'] = 'user/' . $this->get_value($values, 'uid');;
+    }
+    return $data;
+  }
+
+  /**
+   * Overrides FieldPluginBase::render().
+   */
   function render($values) {
     $first = $this->get_value($values);
-    $last = $values->{$this->aliases['last_name']};
+    $last = $this->get_value($values, 'last_name');
 
     switch ($this->options['format']) {
       case 'last_first':
-        return $this->sanitizeValue("$last $first");
+        $output = "$last $first";
       case 'last_c_first':
-        return $this->sanitizeValue("$last, $first");
+        $output = "$last, $first";
       case 'first_last':
-        return $this->sanitizeValue("$first $last");
+        $output = "$first $last";
+    }
+
+    if (isset($output)) {
+      return $this->render_link($this->sanitizeValue($output), $values);
     }
   }
 }
