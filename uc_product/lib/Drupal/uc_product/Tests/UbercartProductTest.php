d441e3a43688aa4841ac8d092c238f80f0d237ed
Dave Long <dave@longwaveconsulting.com>
2013-06-18 09:28:54 +0100

    Convert node-specific hooks to general entity hooks.

--- uc_product/lib/Drupal/uc_product/Tests/UbercartProductTest.php
+++ uc_product/lib/Drupal/uc_product/Tests/UbercartProductTest.php
@@ -69,35 +69,66 @@ class UbercartProductTest extends UbercartTestBase {
         'mm' => t('Millimeters'),
       )),
     );
+    $this->drupalPost('node/add/product', $edit, 'Save');
 
-    $this->drupalPost(
-      'node/add/product',
-      $edit,
-      t('Save')
-    );
-    $this->assertText(
-      t('Product @title has been created.', array('@title' => $edit['title'])),
-      t('Product created.')
-    );
-    $this->assertText(
-      $edit[$body_key],
-      t('Product body found.')
-    );
-    $this->assertText(
-      t('SKU: @model', array('@model' => $edit['model'])),
-      t('Product model found.')
-    );
-    $this->assertText(
-      t('Price: @price', array('@price' => uc_currency_format($edit['sell_price']))),
-      t('Product sell price found.')
-    );
-    $this->assertNoUniqueText(
-      uc_currency_format($edit['sell_price']),
-      t('Price appears more than once.')
-    );
+    $this->assertText(t('Product @title has been created.', array('@title' => $edit['title'])), 'Product created.');
+    $this->assertText($edit[$body_key], 'Product body found.');
+    $this->assertText($edit['model'], 'Product model found.');
+    $this->assertText(uc_currency_format($edit['list_price']), 'Product list price found.');
+    $this->assertText(uc_currency_format($edit['cost']), 'Product cost found.');
+    $this->assertNoUniqueText(uc_currency_format($edit['sell_price']), 'Product sell price found.');
+    $this->assertText(uc_weight_format($edit['weight'], $edit['weight_units']), 'Product weight found.');
+    $this->assertText(uc_length_format($edit['dim_length'], $edit['length_units']), 'Product length found.');
+    $this->assertText(uc_length_format($edit['dim_width'], $edit['length_units']), 'Product width found.');
+    $this->assertText(uc_length_format($edit['dim_height'], $edit['length_units']), 'Product height found.');
 
     $elements = $this->xpath('//body[contains(@class, "uc-product-node")]');
     $this->assertEqual(count($elements), 1, t('Product page contains body CSS class.'));
+
+    // Update the node fields.
+    $edit = array(
+      'title' => $this->randomName(32),
+      $body_key => $this->randomName(64),
+      'model' => $this->randomName(8),
+      'list_price' => mt_rand(1, 200),
+      'cost' => mt_rand(0, 100),
+      'sell_price' => mt_rand(1, 150),
+      'shippable' => mt_rand(0, 1),
+      'weight' => mt_rand(1, 50),
+      'weight_units' => array_rand(array(
+        'lb' => t('Pounds'),
+        'kg' => t('Kilograms'),
+        'oz' => t('Ounces'),
+        'g'  => t('Grams'),
+      )),
+      'dim_length' => mt_rand(1, 50),
+      'dim_width' => mt_rand(1, 50),
+      'dim_height' => mt_rand(1, 50),
+      'length_units' => array_rand(array(
+        'in' => t('Inches'),
+        'ft' => t('Feet'),
+        'cm' => t('Centimeters'),
+        'mm' => t('Millimeters'),
+      )),
+    );
+    $this->clickLink('Edit');
+    $this->drupalPost(NULL, $edit, 'Save');
+
+    $this->assertText(t('Product @title has been updated.', array('@title' => $edit['title'])), 'Product updated.');
+    $this->assertText($edit[$body_key], 'Updated product body found.');
+    $this->assertText($edit['model'], 'Updated product model found.');
+    $this->assertText(uc_currency_format($edit['list_price']), 'Updated product list price found.');
+    $this->assertText(uc_currency_format($edit['cost']), 'Updated product cost found.');
+    $this->assertNoUniqueText(uc_currency_format($edit['sell_price']), 'Updated product sell price found.');
+    $this->assertText(uc_weight_format($edit['weight'], $edit['weight_units']), 'Product weight found.');
+    $this->assertText(uc_length_format($edit['dim_length'], $edit['length_units']), 'Product length found.');
+    $this->assertText(uc_length_format($edit['dim_width'], $edit['length_units']), 'Product width found.');
+    $this->assertText(uc_length_format($edit['dim_height'], $edit['length_units']), 'Product height found.');
+
+    $this->clickLink('Edit');
+    $this->drupalPost(NULL, array(), 'Delete');
+    $this->drupalPost(NULL, array(), 'Delete');
+    $this->assertText(t('Product @title has been deleted.', array('@title' => $edit['title'])), 'Product deleted.');
   }
 
   public function testProductClassForm() {
