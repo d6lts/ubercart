38d88de23f74c0a270c06c572c30f660441e0eb9
Dave Long <dave@longwaveconsulting.com>
2013-11-06 09:08:44 +0000

    Convert flatrate shipping quotes to new form and routing system.

---
+++ shipping/uc_flatrate/lib/Drupal/uc_flatrate/Form/FlatrateEditForm.php
@@ -0,0 +1,114 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\uc_flatrate\Form\FlatrateForm.
+ */
+
+namespace Drupal\uc_flatrate\Form;
+
+use Drupal\Core\Form\FormBase;
+
+/**
+ * Configures the store default flat rate shipping rates.
+ */
+class FlatrateEditForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'uc_flatrate_admin_method_edit_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, array &$form_state, $mid = NULL) {
+    if ($mid && ($method = db_query("SELECT * FROM {uc_flatrate_methods} WHERE mid = :mid", array(':mid' => $mid))->fetchObject())) {
+      $form['mid'] = array(
+        '#type' => 'value',
+        '#value' => $mid,
+      );
+    }
+    else {
+      $mid = 0;
+    }
+
+    $form['title'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Shipping method title'),
+      '#description' => t('The name shown to administrators distinguish this method from other flatrate methods.'),
+      '#default_value' => $mid ? $method->title : '',
+      '#required' => TRUE,
+    );
+    $form['label'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Line item label'),
+      '#description' => t('The name shown to the customer when they choose a shipping method at checkout.'),
+      '#default_value' => $mid ? $method->label : '',
+      '#required' => TRUE,
+    );
+    $form['base_rate'] = array(
+      '#type' => 'uc_price',
+      '#title' => t('Base price'),
+      '#description' => t('The starting price for shipping costs.'),
+      '#default_value' => $mid ? $method->base_rate : '',
+      '#required' => TRUE,
+    );
+    $form['product_rate'] = array(
+      '#type' => 'uc_price',
+      '#title' => t('Default product shipping rate'),
+      '#description' => t('Additional shipping cost per product in cart.'),
+      '#default_value' => $mid ? $method->product_rate : '',
+      '#required' => TRUE,
+    );
+
+    $form['actions'] = array('#type' => 'actions');
+    $form['actions']['submit'] = array(
+      '#type' => 'submit',
+      '#value' => t('Submit'),
+      '#button_type' => 'primary',
+    );
+
+    if ($mid) {
+      $form['actions']['delete'] = array(
+        '#type' => 'submit',
+        '#value' => t('Delete'),
+        '#submit' => array(array($this, 'delete')),
+        '#button_type' => 'danger',
+      );
+    }
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, array &$form_state) {
+    if (isset($form_state['values']['mid'])) {
+      drupal_write_record('uc_flatrate_methods', $form_state['values'], 'mid');
+      drupal_set_message(t('Flat rate shipping method was updated.'));
+      $form_state['redirect'] = 'admin/store/settings/quotes';
+    }
+    else {
+      drupal_write_record('uc_flatrate_methods', $form_state['values']);
+
+      // Ensure Rules picks up the new condition.
+      // entity_flush_caches();
+
+      drupal_set_message(t('Created and enabled new flat rate shipping method.'));
+      $form_state['redirect'] = 'admin/store/settings/quotes';
+      //$form_state['redirect'] = 'admin/store/settings/quotes/manage/get_quote_from_flatrate_' . $form_state['values']['mid'];
+    }
+  }
+
+  /**
+   * Helper function to delete a flatrate method.
+   */
+  public function delete(&$form, &$form_state) {
+    $form_state['redirect'] = 'admin/store/settings/quotes/methods/flatrate/' . $form_state['values']['mid'] . '/delete';
+  }
+
+}
