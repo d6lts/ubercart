545742cf1b06f453e679e2059ce4f0b534459497
Dave Long <dave@longwaveconsulting.com>
2013-11-07 09:00:16 +0000

    Convert address field settings to CMI.

--- uc_store/lib/Drupal/uc_store/Form/AddressFieldsForm.php
+++ uc_store/lib/Drupal/uc_store/Form/AddressFieldsForm.php
@@ -25,6 +25,9 @@ class AddressFieldsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, array &$form_state) {
+    $config = $this->configFactory->get('uc_store.settings')
+      ->get('address_fields');
+
     $form['fields'] = array(
       '#type' => 'table',
       '#header' => array(t('Field'), t('Required'), t('List position')),
@@ -45,28 +48,26 @@ class AddressFieldsForm extends ConfigFormBase {
       'postal_code' => t('Postal code'),
       'phone' => t('Phone number'),
     );
-    $current = variable_get('uc_address_fields', drupal_map_assoc(array('first_name', 'last_name', 'phone', 'company', 'street1', 'street2', 'city', 'zone', 'postal_code', 'country')));
-    $required = variable_get('uc_address_fields_required', drupal_map_assoc(array('first_name', 'last_name', 'street1', 'city', 'zone', 'postal_code', 'country')));
-    $weight = uc_store_address_field_weights();
+
     foreach ($fields as $field => $label) {
       $form['fields'][$field]['#attributes']['class'][] = 'draggable';
-      $form['fields'][$field]['#weight'] = (isset($weight[$field])) ? $weight[$field] : 0;
+      $form['fields'][$field]['#weight'] = $config[$field]['weight'];
       $form['fields'][$field]['status'] = array(
         '#type' => 'checkbox',
         '#title' => $label,
-        '#default_value' => !empty($current[$field]),
+        '#default_value' => $config[$field]['status'],
       );
       $form['fields'][$field]['required'] = array(
         '#type' => 'checkbox',
         '#title' => t('@title is required', array('@title' => $label)),
         '#title_display' => 'invisible',
-        '#default_value' => !empty($required[$field]),
+        '#default_value' => $config[$field]['required'],
       );
       $form['fields'][$field]['weight'] = array(
         '#type' => 'weight',
         '#title' => t('Weight for @title', array('@title' => $label)),
         '#title_display' => 'invisible',
-        '#default_value' => (isset($weight[$field])) ? $weight[$field] : 0,
+        '#default_value' => $config[$field]['weight'],
         '#attributes' => array('class' => array('uc-store-address-fields-weight')),
       );
     }
@@ -79,17 +80,9 @@ class AddressFieldsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, array &$form_state) {
-    $enabled = array();
-    $required = array();
-    $weights = array();
-    foreach (element_children($form['fields']) as $field) {
-      $enabled[$field] = $form_state['values']['fields'][$field]['status'];
-      $required[$field] = $form_state['values']['fields'][$field]['required'];
-      $weights[$field] = $form_state['values']['fields'][$field]['weight'];
-    }
-    variable_set('uc_address_fields', array_filter($enabled));
-    variable_set('uc_address_fields_required', array_filter($required));
-    variable_set('uc_address_fields_weight', $weights);
+    $config = $this->configFactory->get('uc_store.settings')
+      ->set('address_fields', $form_state['values']['fields'])
+      ->save();
 
     parent::submitForm($form, $form_state);
   }
