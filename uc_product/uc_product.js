7b0ecb8ee923991ae0b92cda151ef2144f04e4c6
Dave Long <dave@longwaveconsulting.com>
2013-11-14 18:39:14 +0000

    Convert product node settings into configuration.

--- uc_product/uc_product.js
+++ uc_product/uc_product.js
@@ -7,12 +7,12 @@
 
 Drupal.behaviors.ucProductFieldsetSummaries = {
   attach: function (context) {
-    $('details#edit-uc-product', context).drupalSetSummary(function(context) {
+    $('details#edit-settings-uc-product', context).drupalSetSummary(function(context) {
       var vals = [];
       $('input:checked', context).next('label').each(function() {
         vals.push(Drupal.checkPlain($(this).text()));
       });
-      if (!$('#edit-uc-product-shippable', context).is(':checked')) {
+      if (!$('#edit-settings-uc-product-shippable', context).is(':checked')) {
         vals.unshift(Drupal.t('Not shippable'));
       }
       return vals.join(', ');
