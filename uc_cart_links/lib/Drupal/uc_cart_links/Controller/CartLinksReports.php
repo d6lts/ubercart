79210547475b635b40928325b88fc21a5673598d
Tim Rohaly <tr@202830.no-reply.drupal.org>
2013-11-23 00:29:30 -0800

    Move cart links reports into a controller.

---
+++ uc_cart_links/lib/Drupal/uc_cart_links/Controller/CartLinksReports.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\uc_cart_links\Controller\CartLinksReports.
+ */
+
+namespace Drupal\uc_cart_links\Controller;
+
+/**
+ * Displays the Cart Links report.
+ *
+ * @return
+ *   Renderable form array.
+ */
+class CartLinksReports {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function buildReport() {
+    $header = array(
+      array('data' => t('ID'), 'field' => 'cart_link_id'),
+      array('data' => t('Clicks'), 'field' => 'clicks'),
+      array('data' => t('Last click'), 'field' => 'last_click', 'sort' => 'desc'),
+    );
+
+    $query = db_select('uc_cart_link_clicks')
+      ->extend('Drupal\Core\Database\Query\PagerSelectExtender')
+      ->extend('Drupal\Core\Database\Query\TableSortExtender')
+      ->fields('uc_cart_link_clicks')
+      ->limit(25)
+      ->element(1)
+      ->orderByHeader($header);
+
+    $rows = array();
+    $result = $query->execute();
+    foreach ($result as $data) {
+      $rows[] = array(
+        check_plain($data->cart_link_id),
+        $data->clicks,
+        format_date($data->last_click, 'short'),
+      );
+    }
+
+    $build['report'] = array(
+      '#theme' => 'table',
+      '#header' => $header,
+      '#rows' => $rows,
+      '#empty' => t('No Cart Links have been tracked yet.'),
+    );
+    $build['pager'] = array(
+      '#theme' => 'pager',
+      '#element' => 1,
+    );
+
+    return $build;
+  }
+}
