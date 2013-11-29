a52589d56a80d985cd2a42dbf8ec784f940f9d25
Dave Long <dave@longwaveconsulting.com>
2013-11-29 02:00:31 +0000

    Simplify cart block title theming.

--- uc_cart/js/uc_cart_block.js
+++ uc_cart/js/uc_cart_block.js
@@ -1,18 +1,14 @@
-/**
- * @file
- * Adds effects and behaviors to the cart block.
- */
+(function ($) {
 
 /**
  * Sets the behavior to (un)collapse the cart block on a click
  */
-Drupal.behaviors.ucCollapseBlock = {
-  attach: function(context) {
-    jQuery('.cart-block-title-bar:not(.ucCollapseBlock-processed)', context).addClass('ucCollapseBlock-processed').click(
-      function() {
-        var $items = jQuery('.cart-block-items').toggleClass('collapsed');
-        jQuery('.cart-block-arrow').toggleClass('arrow-down', $items.hasClass('collapsed'));
-      }
-    );
+Drupal.behaviors.uc_cart_block = {
+  attach: function (context) {
+    $(context).find('.cart-block-arrow').once('uc_cart_block').click(function() {
+      $(context).find('.cart-block-arrow, .cart-block-items').toggleClass('collapsed');
+    });
   }
 }
+
+})(jQuery);
