0660b68967712d5fea96849b92151734ce6df1cc
Tim Rohaly <tr@202830.no-reply.drupal.org>
2013-12-08 21:07:24 -0800

    Change variables to configs. Change payment processor callback to a controller. Add routing.yml.

---
+++ payment/uc_2checkout/lib/Drupal/uc_2checkout/Controller/CheckoutController.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\uc_2checkout\Controller\CheckoutController.
+ */
+
+namespace Drupal\uc_2checkout\Controller;
+
+use Drupal\Component\Utility\Unicode;
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+
+/**
+ * Controller routines for uc_2checkout.
+ */
+class CheckoutController extends ControllerBase {
+
+  /**
+   * Finalizes 2checkout transaction.
+   */
+  public function complete($cart_id = 0) {
+    $cart_config = \Drupal::config('uc_cart.settings');
+    $module_config = \Drupal::config('uc_2checkout.settings');
+    watchdog('2Checkout', 'Receiving new order notification for order !order_id.', array('!order_id' => check_plain($_REQUEST['merchant_order_id'])));
+
+    $order = uc_order_load($_REQUEST['merchant_order_id']);
+
+    if (!$order || $order->getStateId() != 'in_checkout') {
+      return t('An error has occurred during payment.  Please contact us to ensure your order has submitted.');
+    }
+
+    $key = $_REQUEST['key'];
+    $order_number = $module_config->get('demo') ? 1 : $_REQUEST['order_number'];
+    $valid = md5($module_config->get('secret_word') . $_REQUEST['sid'] . $order_number . $_REQUEST['total']);
+    if (Unicode::strtolower($key) != Unicode::strtolower($valid)) {
+      uc_order_comment_save($order->id(), 0, t('Attempted unverified 2Checkout completion for this order.'), 'admin');
+      throw new AccessDeniedHttpException();
+    }
+
+    if ($_REQUEST['demo'] == 'Y' xor $module_config->get('demo')) {
+      watchdog('uc_2checkout', 'The 2checkout payment for order <a href="@order_url">@order_id</a> demo flag was set to %flag, but the module is set to %mode mode.', array(
+        '@order_url' => url('admin/store/orders/' . $order->id()),
+        '@order_id' => $order->id(),
+        '%flag' => $_REQUEST['demo'] == 'Y' ? 'Y' : 'N',
+        '%mode' => $module_config->get('demo') ? 'Y' : 'N',
+      ), WATCHDOG_ERROR);
+
+      if (!$module_config->get('demo')) {
+        throw new AccessDeniedHttpException();
+      }
+    }
+
+    $order->billing_street1 = $_REQUEST['street_address'];
+    $order->billing_street2 = $_REQUEST['street_address2'];
+    $order->city = $_REQUEST['city'];
+    $order->billing_postal_code = $_REQUEST['zip'];
+    $order->billing_phone = $_REQUEST['phone'];
+
+    $zone_id = db_query("SELECT zone_id FROM {uc_zones} WHERE zone_code LIKE :code", array(':code' => $_REQUEST['state']))->fetchField();
+    if (!empty($zone_id)) {
+      $order->billing_zone = $zone_id;
+    }
+
+    $country_id = db_query("SELECT country_id FROM {uc_countries} WHERE country_name LIKE :name", array(':name' => $_REQUEST['country']))->fetchField();
+    if (!empty($country_id)) {
+      $order->billing_country = $country_id;
+    }
+
+    // Save changes to order without it's completion.
+    $order->save();
+
+    if (Unicode::strtolower($_REQUEST['email']) !== Unicode::strtolower($order->getEmail())) {
+      uc_order_comment_save($order->id(), 0, t('Customer used a different e-mail address during payment: !email', array('!email' => check_plain($_REQUEST['email']))), 'admin');
+    }
+
+    if ($_REQUEST['credit_card_processed'] == 'Y' && is_numeric($_REQUEST['total'])) {
+      $comment = t('Paid by !type, 2Checkout.com order #!order.', array('!type' => $_REQUEST['pay_method'] == 'CC' ? t('credit card') : t('echeck'), '!order' => check_plain($_REQUEST['order_number'])));
+      uc_payment_enter($order->id(), '2checkout', $_REQUEST['total'], 0, NULL, $comment);
+    }
+    else {
+      drupal_set_message(t('Your order will be processed as soon as your payment clears at 2Checkout.com.'));
+      uc_order_comment_save($order->id(), 0, t('!type payment is pending approval at 2Checkout.com.', array('!type' => $_REQUEST['pay_method'] == 'CC' ? t('Credit card') : t('eCheck'))), 'admin');
+    }
+
+    // Empty that cart...
+    uc_cart_empty($cart_id);
+
+    // Add a comment to let sales team know this came in through the site.
+    uc_order_comment_save($order->id(), 0, t('Order created through website.'), 'admin');
+
+    $build = uc_cart_complete_sale($order, $cart_config->get('new_customer_login'));
+
+    $page = $cart_config->get('checkout_complete_page');
+
+    if (!empty($page)) {
+      drupal_goto($page);
+    }
+
+    return $build;
+  }
+}
