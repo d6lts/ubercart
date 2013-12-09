d6lts/ubercart

commit a06a86a47d0dfb201807e7766963706596f2b3f9

Add visibility qualifiers to views functions. Port some function names to D8.

uc_order/lib/Drupal/uc_order/Plugin/views/field/FullName.php
@@ -71,7 +71,7 @@ class FullName extends FieldPluginBase {
    *
    * Data should be made XSS safe prior to calling this function.
    */
-  function render_link($data, $values) {
+  protected function renderAsLink($data, $values) {
     if (!empty($this->options['link_to_user']) && user_access('access user profiles')) {
       $this->options['alter']['make_link'] = TRUE;
       $this->options['alter']['path'] = 'user/' . $this->getValue($values, 'uid');;
@@ -82,7 +82,7 @@ class FullName extends FieldPluginBase {
   /**
    * Overrides FieldPluginBase::render().
    */
-  function render(ResultRow $values) {
+  public function render(ResultRow $values) {
     $first = $this->getValue($values);
     $last = $this->getValue($values, 'last_name');
 
@@ -96,7 +96,7 @@ class FullName extends FieldPluginBase {
     }
 
     if (isset($output)) {
-      return $this->render_link($this->sanitizeValue($output), $values);
+      return $this->renderAsLink($this->sanitizeValue($output), $values);
     }
   }
 }

uc_order/lib/Drupal/uc_order/Plugin/views/field/Order.php
@@ -61,7 +61,7 @@ class Order extends FieldPluginBase {
    *
    * Data should be made XSS safe prior to calling this function.
    */
-  function render_link($data, $values) {
+  protected function renderAsLink($data, $values) {
     if (!empty($this->options['link_to_order'])) {
       $this->options['alter']['make_link'] = FALSE;
 
@@ -86,9 +86,9 @@ class Order extends FieldPluginBase {
   /**
    * Overrides FieldPluginBase::render().
    */
-  function render(ResultRow $values) {
+  public function render(ResultRow $values) {
     $value = $this->getValue($values);
-    return $this->render_link($this->sanitizeValue($value), $values);
+    return $this->renderAsLink($this->sanitizeValue($value), $values);
   }
 
 }

payment/uc_credit/lib/Drupal/uc_credit/Plugin/views/field/DataField.php
@@ -23,7 +23,7 @@ class DataField extends FieldPluginBase {
   /**
    * Overrides FieldPluginBase::render().
    */
-  function render(ResultRow $values) {
+  public function render(ResultRow $values) {
     // Initialize the encryption key and class.
     $key = uc_credit_encryption_key();
     $crypt = new Encryption();

payment/uc_payment/lib/Drupal/uc_payment/Plugin/views/field/Method.php
@@ -22,7 +22,7 @@ class Method extends FieldPluginBase {
   /**
    * Overrides FieldPluginBase::render().
    */
-  function render(ResultRow $values) {
+  public function render(ResultRow $values) {
     $value = $this->getValue($values);
     return $this->sanitizeValue(_uc_payment_method_data($value, 'name'));
   }

payment/uc_payment/lib/Drupal/uc_payment/Plugin/views/filter/Method.php
@@ -21,7 +21,7 @@ class Method extends InOperator {
   /**
    * Overrides InOperator::getValueOptions().
    */
-  function getValueOptions() {
+  public function getValueOptions() {
     if (!isset($this->value_options)) {
       $this->value_options = array();
 

uc_cart/lib/Drupal/uc_cart/Controller/CheckoutController.php
@@ -123,7 +123,7 @@ class CheckoutController extends ControllerBase {
   /**
    * Allows a customer to review their order before finally submitting it.
    */
-  function review() {
+  public function review() {
     drupal_add_js(drupal_get_path('module', 'uc_cart') . '/js/uc_cart.js');
 
     if (empty($_SESSION['cart_order']) || empty($_SESSION['uc_checkout'][$_SESSION['cart_order']]['do_review'])) {
@@ -172,7 +172,7 @@ class CheckoutController extends ControllerBase {
   /**
    * Completes the sale and finishes checkout.
    */
-  function complete() {
+  public function complete() {
     if (empty($_SESSION['cart_order']) || empty($_SESSION['uc_checkout'][$_SESSION['cart_order']]['do_complete'])) {
       return $this->redirect('uc_cart.cart');
     }

uc_catalog/lib/Drupal/uc_catalog/TreeNode.php
@@ -20,7 +20,7 @@ class TreeNode {
   /**
    * Constructor.
    */
-  function __construct($term = NULL) {
+  public function __construct($term = NULL) {
     if ($term) {
       $this->tid = $term->tid;
       $this->name = $term->name;
@@ -38,7 +38,7 @@ class TreeNode {
    * tree structure. Therefore, if the parameter is more than one level
    * deeper than $this, it should be passed to the last child of $this.
    */
-  function add_child(&$child) {
+  public function add_child(&$child) {
     if ($child->depth - $this->depth == 1) {
       $this->children[] = $child;
     }

uc_order/lib/Drupal/uc_order/Controller/OrderController.php
@@ -19,7 +19,7 @@ class OrderController extends ControllerBase {
   /**
    * Displays an order invoice.
    */
-  function invoice(UcOrderInterface $uc_order, $print = FALSE) {
+  public function invoice(UcOrderInterface $uc_order, $print = FALSE) {
     $build = array(
       '#theme' => 'uc_order_invoice',
       '#order' => $uc_order,
@@ -38,7 +38,7 @@ class OrderController extends ControllerBase {
   /**
    * Displays a log of changes made to an order.
    */
-  function log(UcOrderInterface $uc_order) {
+  public function log(UcOrderInterface $uc_order) {
     $result = db_query("SELECT * FROM {uc_order_log} WHERE order_id = :id ORDER BY created, order_log_id", array(':id' => $uc_order->id()));
 
     $header = array(t('Time'), t('User'), t('Changes'));

commit 0d3554bad85a9969ba2ee28fef2d5e021f033ad4

Commit a06a86a47d0dfb2018 broke the tests. FieldPluginBase::renderAsLink isn't the D8 version of render_link()? Reverted this change in three files.

shipping/uc_shipping/lib/Drupal/uc_shipping/Plugin/views/field/ShipmentId.php
@@ -58,7 +58,7 @@ class ShipmentId extends FieldPluginBase {
    *
    * Data should be made XSS safe prior to calling this function.
    */
-  public function renderAsLink($data, $values) {
+  public function render_link($data, $values) {
     if (!empty($this->options['link_to_shipment'])) {
       $this->options['alter']['make_link'] = FALSE;
 
@@ -81,7 +81,7 @@ class ShipmentId extends FieldPluginBase {
    * Overrides views_handler_field::render().
    */
   public function render($values) {
-    return $this->renderAsLink(check_plain($values->{$this->field_alias}), $values);
+    return $this->render_link(check_plain($values->{$this->field_alias}), $values);
   }
 
 }

uc_order/lib/Drupal/uc_order/Plugin/views/field/FullName.php
@@ -71,7 +71,7 @@ class FullName extends FieldPluginBase {
    *
    * Data should be made XSS safe prior to calling this function.
    */
-  protected function renderAsLink($data, $values) {
+  protected function render_link($data, $values) {
     if (!empty($this->options['link_to_user']) && user_access('access user profiles')) {
       $this->options['alter']['make_link'] = TRUE;
       $this->options['alter']['path'] = 'user/' . $this->getValue($values, 'uid');;
@@ -96,7 +96,7 @@ class FullName extends FieldPluginBase {
     }
 
     if (isset($output)) {
-      return $this->renderAsLink($this->sanitizeValue($output), $values);
+      return $this->render_link($this->sanitizeValue($output), $values);
     }
   }
 }

uc_order/lib/Drupal/uc_order/Plugin/views/field/Order.php
@@ -61,7 +61,7 @@ class Order extends FieldPluginBase {
    *
    * Data should be made XSS safe prior to calling this function.
    */
-  protected function renderAsLink($data, $values) {
+  protected function render_link($data, $values) {
     if (!empty($this->options['link_to_order'])) {
       $this->options['alter']['make_link'] = FALSE;
 
@@ -88,7 +88,7 @@ class Order extends FieldPluginBase {
    */
   public function render(ResultRow $values) {
     $value = $this->getValue($values);
-    return $this->renderAsLink($this->sanitizeValue($value), $values);
+    return $this->render_link($this->sanitizeValue($value), $values);
   }
 
 }