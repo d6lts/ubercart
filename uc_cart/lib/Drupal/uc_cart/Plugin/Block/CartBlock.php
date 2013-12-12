fa326b42cb5996942c8853f9fd437e1a83c723d7
Dave Long <dave@longwaveconsulting.com>
2013-12-12 00:53:25 +0000

    Issue #2153613: Declare JavaScript dependencies.

--- uc_cart/lib/Drupal/uc_cart/Plugin/Block/CartBlock.php
+++ uc_cart/lib/Drupal/uc_cart/Plugin/Block/CartBlock.php
@@ -137,6 +137,8 @@ class CartBlock extends BlockBase {
 
       // If the block is collapsible, add the appropriate JS.
       if ($this->configuration['collapsible']) {
+        $build['#attached']['library'][] = array('system', 'drupal');
+        $build['#attached']['library'][] = array('system', 'jquery.once');
         $build['#attached']['js'][] = drupal_get_path('module', 'uc_cart') . '/js/uc_cart_block.js';
       }
 
