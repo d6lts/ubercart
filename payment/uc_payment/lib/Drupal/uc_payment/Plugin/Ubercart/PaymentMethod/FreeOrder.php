ab43f8c152b733ef7f4a631fd7c023adb5b1a2df
Dave Long <dave@longwaveconsulting.com>
2013-12-30 12:27:16 +0000

    Convert free order payment method to plugin.

---
+++ payment/uc_payment/lib/Drupal/uc_payment/Plugin/Ubercart/PaymentMethod/FreeOrder.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\uc_payment\Plugin\Ubercart\PaymentMethod\FreeOrder.
+ */
+
+namespace Drupal\uc_payment\Plugin\Ubercart\PaymentMethod;
+
+use Drupal\uc_order\UcOrderInterface;
+use Drupal\uc_payment\PaymentMethodPluginBase;
+
+/**
+ * Defines a free order payment method.
+ *
+ * @Plugin(
+ *   id = "free_order",
+ *   name = @Translation("Free order"),
+ *   title = @Translation("No payment required"),
+ *   description = @Translation("Allow customers with free orders to check out without paying."),
+ *   checkout = TRUE,
+ *   no_gateway = TRUE,
+ *   weight = 0,
+ * )
+ */
+class FreeOrder extends PaymentMethodPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function cartDetails(UcOrderInterface $order, array $form, array &$form_state) {
+    return array(
+      '#markup' => t('Continue with checkout to complete your order.'),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function orderSubmit(UcOrderInterface $order) {
+    if ($order->getTotal() >= 0.01) {
+      return array(array(
+        'pass' => FALSE,
+        'message' => t('We cannot process your order without payment.'),
+      ));
+    }
+
+    uc_payment_enter($order->id(), 'free_order', 0, 0, NULL, t('Checkout completed for a free order.'));
+  }
+
+}
