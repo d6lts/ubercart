9f48c45930ddcf6d9fa8e3f4d73435245c8dfcc9
Dave Long <dave@longwaveconsulting.com>
2014-09-19 20:26:23 +0100

    Add cacheability metadata to access checks.

--- uc_order/src/UcOrderAccessControlHandler.php
+++ uc_order/src/UcOrderAccessControlHandler.php
@@ -7,6 +7,7 @@
 
 namespace Drupal\uc_order;
 
+use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityAccessControlHandler;
 use Drupal\Core\Session\AccountInterface;
@@ -19,51 +20,48 @@ class UcOrderAccessControlHandler extends EntityAccessControlHandler {
   /**
    * {@inheritdoc}
    */
-  protected function checkAccess(EntityInterface $entity, $operation, $langcode, AccountInterface $account) {
+  protected function checkAccess(EntityInterface $order, $operation, $langcode, AccountInterface $account) {
     switch ($operation) {
       case 'view':
       case 'invoice':
         // Admins can view all orders.
         if ($account->hasPermission('view all orders')) {
-          return TRUE;
+          return AccessResult::allowed()->cachePerRole();
         }
         // Non-anonymous users can view their own orders and invoices with permission.
         $permission = $operation == 'view' ? 'view own orders' : 'view own invoices';
-        if ($account->id() && $account->id() == $entity->getUserId() && $account->hasPermission($permission)) {
-          return TRUE;
+        if ($account->id() && $account->id() == $order->getUserId() && $account->hasPermission($permission)) {
+          return AccessResult::allowed()->cachePerRole()->cachePerUser()->cacheUntilEntityChanges($order);
         }
-        return FALSE;
-        break;
+        return AccessResult::forbidden()->cachePerRole()->cachePerUser()->cacheUntilEntityChanges($order);
 
       case 'update':
-        return $account->hasPermission('edit orders');
-        break;
+        return AccessResult::allowedIfHasPermission($account, 'edit orders')->cachePerRole()->cachePerUser();
 
       case 'delete':
         if ($account->hasPermission('unconditionally delete orders')) {
           // Unconditional deletion perms are always TRUE.
-          return TRUE;
+          return AccessResult::allowed()->cachePerRole()->cachePerUser();
         }
         if ($account->hasPermission('delete orders')) {
           // Only users with unconditional deletion perms can delete completed orders.
-          if ($entity->getStateId() == 'completed') {
-            return FALSE;
+          if ($order->getStateId() == 'completed') {
+            return AccessResult::forbidden()->cachePerRole()->cachePerUser()->cacheUntilEntityChanges($order);
           }
           else {
             // See if any modules have a say in this order's eligibility for deletion.
             $module_handler = \Drupal::moduleHandler();
             foreach ($module_handler->getImplementations('uc_order_can_delete') as $module) {
               $function = $module . '_uc_order_can_delete';
-              if ($function($entity) === FALSE) {
-                return FALSE;
+              if ($function($order) === FALSE) {
+                return AccessResult::forbidden()->cachePerRole()->cachePerUser()->cacheUntilEntityChanges($order);
               }
             }
 
-            return TRUE;
+            return AccessResult::allowed()->cachePerRole()->cachePerUser()->cacheUntilEntityChanges($order);
           }
         }
-        return FALSE;
-        break;
+        return AccessResult::forbidden()->cachePerRole()->cachePerUser()->cacheUntilEntityChanges($order);
     }
   }
 
@@ -71,7 +69,7 @@ class UcOrderAccessControlHandler extends EntityAccessControlHandler {
    * {@inheritdoc}
    */
   protected function checkCreateAccess(AccountInterface $account, array $context, $entity_bundle = NULL) {
-    return $account->hasPermission('create orders');
+    return AccessResult::allowedIfHasPermission($account, 'create orders')->cachePerRole();
   }
 
 }
