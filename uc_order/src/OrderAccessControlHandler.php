6ec9dbac78d94ec571ae66ce8072965b88527f4c
Tim Rohaly <tr@202830.no-reply.drupal.org>
2015-03-27 02:33:00 -0700

    Core issue #2459373: cachePerRole() replaced by cachePerPermissions().

--- uc_order/src/OrderAccessControlHandler.php
+++ uc_order/src/OrderAccessControlHandler.php
@@ -26,27 +26,27 @@ class OrderAccessControlHandler extends EntityAccessControlHandler {
       case 'invoice':
         // Admins can view all orders.
         if ($account->hasPermission('view all orders')) {
-          return AccessResult::allowed()->cachePerRole();
+          return AccessResult::allowed()->cachePerPermissions();
         }
         // Non-anonymous users can view their own orders and invoices with permission.
         $permission = $operation == 'view' ? 'view own orders' : 'view own invoices';
         if ($account->id() && $account->id() == $order->getUserId() && $account->hasPermission($permission)) {
-          return AccessResult::allowed()->cachePerRole()->cachePerUser()->cacheUntilEntityChanges($order);
+          return AccessResult::allowed()->cachePerPermissions()->cachePerUser()->cacheUntilEntityChanges($order);
         }
-        return AccessResult::forbidden()->cachePerRole()->cachePerUser()->cacheUntilEntityChanges($order);
+        return AccessResult::forbidden()->cachePerPermissions()->cachePerUser()->cacheUntilEntityChanges($order);
 
       case 'update':
-        return AccessResult::allowedIfHasPermission($account, 'edit orders')->cachePerRole()->cachePerUser();
+        return AccessResult::allowedIfHasPermission($account, 'edit orders')->cachePerPermissions()->cachePerUser();
 
       case 'delete':
         if ($account->hasPermission('unconditionally delete orders')) {
           // Unconditional deletion perms are always TRUE.
-          return AccessResult::allowed()->cachePerRole()->cachePerUser();
+          return AccessResult::allowed()->cachePerPermissions()->cachePerUser();
         }
         if ($account->hasPermission('delete orders')) {
           // Only users with unconditional deletion perms can delete completed orders.
           if ($order->getStateId() == 'completed') {
-            return AccessResult::forbidden()->cachePerRole()->cachePerUser()->cacheUntilEntityChanges($order);
+            return AccessResult::forbidden()->cachePerPermissions()->cachePerUser()->cacheUntilEntityChanges($order);
           }
           else {
             // See if any modules have a say in this order's eligibility for deletion.
@@ -54,14 +54,14 @@ class OrderAccessControlHandler extends EntityAccessControlHandler {
             foreach ($module_handler->getImplementations('uc_order_can_delete') as $module) {
               $function = $module . '_uc_order_can_delete';
               if ($function($order) === FALSE) {
-                return AccessResult::forbidden()->cachePerRole()->cachePerUser()->cacheUntilEntityChanges($order);
+                return AccessResult::forbidden()->cachePerPermissions()->cachePerUser()->cacheUntilEntityChanges($order);
               }
             }
 
-            return AccessResult::allowed()->cachePerRole()->cachePerUser()->cacheUntilEntityChanges($order);
+            return AccessResult::allowed()->cachePerPermissions()->cachePerUser()->cacheUntilEntityChanges($order);
           }
         }
-        return AccessResult::forbidden()->cachePerRole()->cachePerUser()->cacheUntilEntityChanges($order);
+        return AccessResult::forbidden()->cachePerPermissions()->cachePerUser()->cacheUntilEntityChanges($order);
     }
   }
 
@@ -69,7 +69,7 @@ class OrderAccessControlHandler extends EntityAccessControlHandler {
    * {@inheritdoc}
    */
   protected function checkCreateAccess(AccountInterface $account, array $context, $entity_bundle = NULL) {
-    return AccessResult::allowedIfHasPermission($account, 'create orders')->cachePerRole();
+    return AccessResult::allowedIfHasPermission($account, 'create orders')->cachePerPermissions();
   }
 
 }
