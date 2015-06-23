dba01690600ecef42d36d407ff471f0e74b56b5c
Tim Rohaly <tr@202830.no-reply.drupal.org>
2015-06-23 12:06:03 -0700

    Core issue [#2328205]: taxonomy_term_load_parents_all() removed.

--- uc_catalog/src/CatalogBreadcrumbBuilder.php
+++ uc_catalog/src/CatalogBreadcrumbBuilder.php
@@ -75,7 +75,7 @@ class CatalogBreadcrumbBuilder implements BreadcrumbBuilderInterface {
   protected function catalogBreadcrumb($node) {
     $breadcrumb[] = Link::createFromRoute($this->t('Home'), '<front>');
     $breadcrumb[] = new Link(t('Catalog'), Url::fromRoute('view.uc_catalog.page_1'));
-    if ($parents = taxonomy_term_load_parents_all($node->taxonomy_catalog->target_id)) {
+    if ($parents = \Drupal::entityManager()->getStorage('taxonomy_term')->loadAllParents($node->taxonomy_catalog->target_id)) {
       $parents = array_reverse($parents);
       foreach ($parents as $parent) {
         $breadcrumb[] = new Link($parent->label(), Url::fromRoute('view.uc_catalog.page_1', ['term_node_tid_depth' => $parent->id()]));
@@ -90,7 +90,7 @@ class CatalogBreadcrumbBuilder implements BreadcrumbBuilderInterface {
   protected function catalogTermBreadcrumb($tid) {
     $breadcrumb[] = Link::createFromRoute($this->t('Home'), '<front>');
     $breadcrumb[] = new Link(t('Catalog'), Url::fromRoute('view.uc_catalog.page_1'));
-    if ($parents = taxonomy_term_load_parents_all($tid)) {
+    if ($parents = \Drupal::entityManager()->getStorage('taxonomy_term')->loadAllParents($tid)) {
       array_shift($parents);
       $parents = array_reverse($parents);
       foreach ($parents as $parent) {
