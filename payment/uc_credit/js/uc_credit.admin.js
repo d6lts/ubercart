a9e24518a9b3070dd7bd688a6e744a7b06b6087c
Tim Rohaly <tr@202830.no-reply.drupal.org>
2016-01-23 14:57:47 -0800

    JavaScript work: Make them pass eslint, add documentation comments, 'use strict'.

--- payment/uc_credit/js/uc_credit.admin.js
+++ payment/uc_credit/js/uc_credit.admin.js
@@ -5,14 +5,23 @@
 
 (function ($) {
 
-Drupal.behaviors.creditAdminFieldsetSummaries = {
-  attach: function (context) {
-    $('details#edit-cc-security', context).drupalSetSummary(function(context) {
-      return Drupal.t('Encryption key path') + ': '
-        + $('#edit-uc-credit-encryption-path', context).val();
-    });
+  'use strict';
 
-  }
-};
+  /**
+   * Provide the summary information for the credit card settings vertical tabs.
+   *
+   * @type {Drupal~behavior}
+   *
+   * @prop {Drupal~behaviorAttach} attach
+   *   Attaches the behavior for the credit card settings summaries.
+   */
+  Drupal.behaviors.creditAdminFieldsetSummaries = {
+    attach: function (context) {
+      $('details#edit-cc-security', context).drupalSetSummary(function (context) {
+        return Drupal.t('Encryption key path') + ': '
+          + $('#edit-uc-credit-encryption-path', context).val();
+      });
+    }
+  };
 
 })(jQuery);
--- shipping/uc_usps/js/uc_usps.admin.js
+++ shipping/uc_usps/js/uc_usps.admin.js
@@ -5,26 +5,36 @@
 
 (function ($) {
 
-Drupal.behaviors.uspsAdminFieldsetSummaries = {
-  attach: function (context) {
-    $('details#edit-domestic', context).drupalSetSummary(function(context) {
-      if ($('#edit-uc-usps-online-rates').is(':checked')) {
-        return Drupal.t('Using "online" rates');
-      }
-      else {
-        return Drupal.t('Using standard rates');
-      }
-    });
+  'use strict';
 
-    $('details#edit-uc-usps-markups', context).drupalSetSummary(function(context) {
-      return Drupal.t('Rate markup') + ': '
-        + $('#edit-uc-usps-rate-markup', context).val() + ' '
-        + $('#edit-uc-usps-rate-markup-type', context).val() + '<br />'
-        + Drupal.t('Weight markup') + ': '
-        + $('#edit-uc-usps-weight-markup', context).val() + ' '
-        + $('#edit-uc-usps-weight-markup-type', context).val();
-    });
-  }
-};
+  /**
+   * Provide the summary information for the USPS settings vertical tabs.
+   *
+   * @type {Drupal~behavior}
+   *
+   * @prop {Drupal~behaviorAttach} attach
+   *   Attaches the behavior for the USPS settings summaries.
+   */
+  Drupal.behaviors.uspsAdminFieldsetSummaries = {
+    attach: function (context) {
+      $('details#edit-domestic', context).drupalSetSummary(function (context) {
+        if ($('#edit-uc-usps-online-rates').is(':checked')) {
+          return Drupal.t('Using "online" rates');
+        }
+        else {
+          return Drupal.t('Using standard rates');
+        }
+      });
+
+      $('details#edit-uc-usps-markups', context).drupalSetSummary(function (context) {
+        return Drupal.t('Rate markup') + ': '
+          + $('#edit-uc-usps-rate-markup', context).val() + ' '
+          + $('#edit-uc-usps-rate-markup-type', context).val() + '<br />'
+          + Drupal.t('Weight markup') + ': '
+          + $('#edit-uc-usps-weight-markup', context).val() + ' '
+          + $('#edit-uc-usps-weight-markup-type', context).val();
+      });
+    }
+  };
 
 })(jQuery);
