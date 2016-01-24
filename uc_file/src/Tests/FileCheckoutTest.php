d21463aa97f9efbd0b1caca4dcfe21c7ac1eed33
Tim Rohaly <tr@202830.no-reply.drupal.org>
2016-01-24 13:27:09 -0800

    Clean up FileCheckout test.

--- uc_file/src/Tests/FileCheckoutTest.php
+++ uc_file/src/Tests/FileCheckoutTest.php
@@ -12,7 +12,7 @@ use Drupal\uc_file\Tests\FileTestBase;
 use Drupal\user\Entity\User;
 
 /**
- * Tests file assignment upon checkout.
+ * Tests file download upon checkout.
  *
  * @group Ubercart
  */
@@ -36,11 +36,11 @@ class FileCheckoutTest extends FileTestBase {
       ->save();
   }
 
-  public function testCheckoutFileAssignment() {
+  public function testCheckoutFileDownload() {
     $this->drupalLogin($this->adminUser);
     $method = $this->createPaymentMethod('other');
 
-    // Add file assignment to the test product.
+    // Add file download to the test product.
     $filename = $this->getTestFile();
     $this->drupalPostForm('node/' . $this->product->id() . '/edit/features', array('feature' => 'file'), t('Add'));
     $this->drupalPostForm(NULL, array('uc_file_filename' => $filename), t('Save feature'));
@@ -56,18 +56,23 @@ class FileCheckoutTest extends FileTestBase {
 
     // Find the order uid.
     $uid = db_query('SELECT uid FROM {uc_orders} ORDER BY order_id DESC')->fetchField();
-    $account = User::load($uid);
+
     // @todo Re-enable when Rules is available.
+    // $account = User::load($uid);
     // $this->assertTrue($account->hasFile($fid), 'New user was granted file.');
     $order = Order::load($order->id());
     $this->assertEqual($order->getStatusId(), 'payment_received', 'Shippable order was set to payment received.');
 
-    // 4 e-mails: new account, customer invoice, admin invoice, file assignment
+    // Test that the file shows up on the user's purchased files list.
+    //$this->drupalGet('user/' . $uid . '/purchased-files');
+    //$this->assertText($filename, 'File found in list of purchased files.');
+
+    // 4 e-mails: new account, customer invoice, admin invoice, file download.
     $this->assertMailString('subject', 'Account details', 4, 'New account email was sent');
     $this->assertMailString('subject', 'Your Order at Ubercart', 4, 'Customer invoice was sent');
     $this->assertMailString('subject', 'New Order at Ubercart', 4, 'Admin notification was sent');
     // @todo Re-enable when Rules is available.
-    // $this->assertMailString('subject', 'file granted', 4, 'File assignment notification was sent');
+    // $this->assertMailString('subject', 'File Downloads', 4, 'File download notification was sent');
 
     \Drupal::state()->set('system.test_email_collector', []);
 
@@ -86,12 +91,12 @@ class FileCheckoutTest extends FileTestBase {
     $order = Order::load($order->id());
     $this->assertEqual($order->getStatusId(), 'completed', 'Non-shippable order was set to completed.');
 
-    // 3 e-mails: customer invoice, admin invoice, file assignment
-    $this->assertNoMailString('subject', 'Account details', 4, 'New account email was sent');
-    $this->assertMailString('subject', 'Your Order at Ubercart', 4, 'Customer invoice was sent');
-    $this->assertMailString('subject', 'New Order at Ubercart', 4, 'Admin notification was sent');
+    // 3 e-mails: customer invoice, admin invoice, file download.
+    $this->assertNoMailString('subject', 'Account details', 3, 'New account email was sent');
+    $this->assertMailString('subject', 'Your Order at Ubercart', 3, 'Customer invoice was sent');
+    $this->assertMailString('subject', 'New Order at Ubercart', 3, 'Admin notification was sent');
     // @todo Re-enable when Rules is available.
-    // $this->assertMailString('subject', 'file granted', 4, 'File assignment notification was sent');
+    // $this->assertMailString('subject', 'File Downloads', 3, 'File download notification was sent');
   }
 
 }
