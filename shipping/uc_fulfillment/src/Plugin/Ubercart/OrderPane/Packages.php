108ec9d277cf2611fe80f56c2c24aebc73cc5fa7
Tim Rohaly <tr@202830.no-reply.drupal.org>
2016-01-30 02:12:05 -0800

    Issue #2659278 by TR: Convert fulfillment order pane into plugin

---
+++ shipping/uc_fulfillment/src/Plugin/Ubercart/OrderPane/Packages.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\uc_fulfillment\Plugin\Ubercart\OrderPane\Packages.
+ */
+
+namespace Drupal\uc_fulfillment\Plugin\Ubercart\OrderPane;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\uc_order\OrderPanePluginBase;
+use Drupal\uc_order\OrderInterface;
+
+/**
+ * Display tracking numbers of shipped packages.
+ *
+ * @UbercartOrderPane(
+ *   id = "packages",
+ *   title = @Translation("Tracking numbers"),
+ *   weight = 7,
+ * )
+ */
+class Packages extends OrderPanePluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getClasses() {
+    return 'pos-left';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function view(OrderInterface $order, $view_mode) {
+    if ($view_mode == 'customer' || $view_mode == 'view') {
+      $tracking = array();
+      $result = db_query('SELECT sid FROM {uc_shipments} WHERE order_id = :id', [':id' => $order->id()]);
+      foreach ($result as $shipment) {
+        $shipment = uc_fulfillment_shipment_load($shipment->sid);
+        if ($shipment->tracking_number) {
+          $tracking[$shipment->carrier]['data'] = $shipment->carrier;
+          $tracking[$shipment->carrier]['children'][] = $shipment->tracking_number;
+        }
+        else {
+          foreach ($shipment->packages as $package) {
+            if ($package->tracking_number) {
+              $tracking[$shipment->carrier]['data'] = $shipment->carrier;
+              $tracking[$shipment->carrier]['children'][] = $package->tracking_number;
+            }
+          }
+        }
+      }
+
+      // Do not show an empty pane to customers.
+      if ($op == 'view' || !empty($tracking)) {
+        $build['tracking'] = array(
+          '#theme' => 'item_list',
+          '#items' => $tracking,  // @todo #plain_text ?
+        );
+
+        return $build;
+      }
+      break;
+    }
+  }
+
+}
