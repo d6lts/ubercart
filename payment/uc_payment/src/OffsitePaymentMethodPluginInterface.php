e8d039d05f5e52cdb017445904abe378cb22abd0
Dave Long <dave@longwaveconsulting.com>
2016-02-05 08:35:50 +0000

    Add an offsite payment method plugin interface, and convert PayPal WPS to use it.

---
+++ payment/uc_payment/src/OffsitePaymentMethodPluginInterface.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\uc_payment\OffsitePaymentMethodPluginInterface.
+ */
+
+namespace Drupal\uc_payment;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\uc_order\OrderInterface;
+
+/**
+ * Defines an interface for payment method plugins that redirect off-site.
+ */
+interface OffsitePaymentMethodPluginInterface extends PaymentMethodPluginInterface {
+
+  /**
+   * Form constructor.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   * @param \Drupal\uc_order\OrderInterface
+   *   The order that is being processed.
+   *
+   * @return array
+   *   The form structure.
+   */
+  public function buildRedirectForm(array $form, FormStateInterface $form_state, OrderInterface $order);
+
+}
