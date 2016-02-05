709e4e8f08c255e4be8bba057dc77a4e80eef375
Dave Long <dave@longwaveconsulting.com>
2016-02-05 08:58:52 +0000

    Convert 2Checkout to use OffsitePaymentMethodPluginInterface.

--- payment/uc_2checkout/src/Plugin/Ubercart/PaymentMethod/TwoCheckout.php
+++ payment/uc_2checkout/src/Plugin/Ubercart/PaymentMethod/TwoCheckout.php
@@ -7,9 +7,11 @@
 
 namespace Drupal\uc_2checkout\Plugin\Ubercart\PaymentMethod;
 
+use Drupal\Component\Utility\Unicode;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Url;
 use Drupal\uc_order\OrderInterface;
+use Drupal\uc_payment\OffsitePaymentMethodPluginInterface;
 use Drupal\uc_payment\PaymentMethodPluginBase;
 
 /**
@@ -21,7 +23,7 @@ use Drupal\uc_payment\PaymentMethodPluginBase;
  *   redirect = "\Drupal\uc_2checkout\Form\TwoCheckoutForm",
  * )
  */
-class TwoCheckout extends PaymentMethodPluginBase {
+class TwoCheckout extends PaymentMethodPluginBase implements OffsitePaymentMethodPluginInterface {
 
   /**
    * {@inheritdoc}
@@ -204,6 +206,70 @@ class TwoCheckout extends PaymentMethodPluginBase {
     }
 
     return $review;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildRedirectForm(array $form, FormStateInterface $form_state, OrderInterface $order = NULL) {
+    $country = \Drupal::service('country_manager')->getCountry($order->getAddress('billing')->country);
+
+    $data = array(
+      'sid' => $this->configuration['sid'],
+      'mode' => '2CO',
+      'card_holder_name' => Unicode::substr($order->getAddress('billing')->first_name . ' ' . $order->getAddress('billing')->last_name, 0, 128),
+      'street_address' => Unicode::substr($order->getAddress('billing')->street1, 0, 64),
+      'street_address2' => Unicode::substr($order->getAddress('billing')->street2, 0, 64),
+      'city' => Unicode::substr($order->getAddress('billing')->city, 0, 64),
+      'state' => $order->getAddress('billing')->zone,
+      'zip' => Unicode::substr($order->getAddress('billing')->postal_code, 0, 16),
+      'country' => $country ? $country->getAlpha3() : 'USA',
+      'email' => Unicode::substr($order->getEmail(), 0, 64),
+      'phone' => Unicode::substr($order->getAddress('billing')->phone, 0, 16),
+      'purchase_step' => 'payment-method',
+
+      'demo' => $this->configuration['demo'] ? 'Y' : 'N',
+      'lang' => $this->configuration['language'],
+      'merchant_order_id' => $order->id(),
+      'pay_method' => 'CC',
+      'x_receipt_link_url' => Url::fromRoute('uc_2checkout.complete', ['cart_id' => \Drupal::service('uc_cart.manager')->get()->getId()], ['absolute' => TRUE])->toString(),
+
+      'total' => uc_currency_format($order->getTotal(), FALSE, FALSE, '.'),
+      'cart_order_id' => $order->id(),
+    );
+
+    if ($currency_code = $this->configuration['currency_code']) {
+      $data['currency_code'] = $currency_code;
+    }
+
+    $i = 0;
+    $order->products = \Drupal::entityTypeManager()->getStorage('uc_order_product')->loadByProperties(['order_id' => $order->id()]);
+    foreach ($order->products as $product) {
+      $i++;
+      $data['li_' . $i . '_type'] = 'product';
+      $data['li_' . $i . '_name'] = $product->title->value; // @todo: HTML escape and limit to 128 chars
+      $data['li_' . $i . '_quantity'] = $product->qty->value;
+      $data['li_' . $i . '_product_id'] = $product->model->value;
+      $data['li_' . $i . '_price'] = uc_currency_format($product->price->value, FALSE, FALSE, '.');
+    }
+
+    if ('direct' == $this->configuration['checkout_type']) {
+      $form['#attached']['library'][] = 'uc_2checkout/2checkout.direct';
+    }
+
+    $form['#action'] = $this->configuration['server_url'];
+
+    foreach ($data as $name => $value) {
+      $form[$name] = array('#type' => 'hidden', '#value' => $value);
+    }
+
+    $form['actions'] = array('#type' => 'actions');
+    $form['actions']['submit'] = array(
+      '#type' => 'submit',
+      '#value' => $this->t('Submit order'),
+    );
+
+    return $form;
   }
 
 }
