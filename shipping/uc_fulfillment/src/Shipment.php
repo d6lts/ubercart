ed8383fa532635438e0b6e4d189d03c3a39c9448
Tim Rohaly <tr@202830.no-reply.drupal.org>
2016-02-07 21:29:25 -0800

    Add currency code to the uc_shipments schema (can't store a cost without knowing the units...). Add phone and e-mail to both the origin and destination addresses stored in the uc_shipments table.

--- shipping/uc_fulfillment/src/Shipment.php
+++ shipping/uc_fulfillment/src/Shipment.php
@@ -448,6 +448,7 @@ class Shipment implements ShipmentInterface {
       'ship_date' => $this->ship_date,
       'expected_delivery' => $this->expected_delivery,
       'cost' => $this->cost,
+      'currency' => $this->currency,
       'changed' => $this->changed,
     );
     if (!isset($this->sid)) {
