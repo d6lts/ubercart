b6d27c8791083c52c3e541920ea59754106867da
Tim Rohaly <tr@202830.no-reply.drupal.org>
2016-02-17 02:52:29 -0800

    Some new tests for making shipments.

--- shipping/uc_fulfillment/src/Tests/ShipmentTest.php
+++ shipping/uc_fulfillment/src/Tests/ShipmentTest.php
@@ -116,8 +116,211 @@ class ShipmentTest extends UbercartTestBase {
 
     // Test reaching this through the shipments tab too ...
 
+    // Select all packages and create shipment using the default "Manual" method..
+    $this->drupalPostForm(
+      NULL,
+      array('shipping_types[small_package][table][' . $order->id() . '][checked]' => 1),
+      t('Ship packages')
+    );
+    // Check that we're now on the shipment details page.
+    $this->assertUrl('admin/store/orders/' . $order->id() . '/ship?method_id=manual&0=1');
+    $this->assertText(
+      'Origin address',
+      'Origin address pane found.'
+    );
+    $this->assertText(
+      'Destination address',
+      'Destination address pane found.'
+    );
+    $this->assertText(
+      'Package 1',
+      'Packages data pane found.'
+    );
+    $this->assertText(
+      'Shipment data',
+      'Shipment data pane found.'
+    );
 
-    // Check View, Edit, Delete, Print, and Packing Slip actions and tabs.
+    $street = array_flip(array(
+      'Street',
+      'Avenue',
+      'Place',
+      'Way',
+      'Road',
+      'Boulevard',
+      'Court',
+    ));
+
+    // Fill in the details and make the shipment.
+    // If we filled the addresses in when we created the order,
+    // those values should already be set here so we wouldn't
+    // have to fill them in again.
+    $form_values = array(
+      'pickup_address[first_name]' => $this->randomMachineName(6),
+      'pickup_address[last_name]' => $this->randomMachineName(12),
+      'pickup_address[company]' => $this->randomMachineName(10) . ', Inc.',
+      'pickup_address[street1]' => mt_rand(10, 1000) . ' ' .
+                                   $this->randomMachineName(10) . ' ' .
+                                   array_rand($street),
+      'pickup_address[street2]' => 'Suite ' . mt_rand(100, 999),
+      'pickup_address[city]' => $this->randomMachineName(10),
+      'pickup_address[postal_code]' => mt_rand(10000, 99999),
+      'delivery_address[first_name]' => $this->randomMachineName(6),
+      'delivery_address[last_name]' => $this->randomMachineName(12),
+      'delivery_address[company]' => $this->randomMachineName(10) . ', Inc.',
+      'delivery_address[street1]' => mt_rand(10, 1000) . ' ' .
+                                     $this->randomMachineName(10) . ' ' .
+                                     array_rand($street),
+      'delivery_address[street2]' => 'Suite ' . mt_rand(100, 999),
+      'delivery_address[city]' => $this->randomMachineName(10),
+      'delivery_address[postal_code]' => mt_rand(10000, 99999),
+      'packages[1][pkg_type]' => 'envelope',
+      'packages[1][declared_value]' => '1234.56',
+      'packages[1][tracking_number]' => '4-8-15-16-23-42',
+      'packages[1][weight][weight]' => '3',
+      'packages[1][weight][units]' => array_rand(array_flip(array('lb', 'kg', 'oz', 'g'))),
+      'packages[1][dimensions][length]' => '1',
+      'packages[1][dimensions][width]' => '1',
+      'packages[1][dimensions][height]' => '1',
+      'packages[1][dimensions][length]' => '1',
+      'packages[1][dimensions][units]' => array_rand(array_flip(array('in', 'ft', 'cm', 'mm'))),
+      'carrier' => 'FedEx',
+      'accessorials' => 'Standard Overnight',
+      'transaction_id' => 'THX1138',
+      'tracking_number' => '1234567890ABCD',
+      'ship_date[date]' => '1985-10-26',
+      'expected_delivery[date]' => '2015-10-21',
+      'cost' => '12.34',
+    );
+
+//@todo fix ajax and uncomment settings country and zone.
+    // Find available countries for our select.
+    $country_ids = \Drupal::service('country_manager')->getEnabledList();
+    $pickup_country = array_rand($country_ids);
+//    $this->drupalPostAjaxForm(NULL, ['pickup_address[country]' => $pickup_country], 'pickup_address[country]');
+
+    // Don't try to set the zone unless the country has zones!
+    $zone_list = \Drupal::service('country_manager')->getZoneList($pickup_country);
+    if (!empty($zone_list)) {
+ //     $form_values['pickup_address[zone]'] = array_rand($zone_list);
+    }
+
+    $delivery_country = array_rand($country_ids);
+//    $this->drupalPostAjaxForm(NULL, ['delivery_address[country]' => $delivery_country], 'delivery_address[country]');
+    $zone_list = \Drupal::service('country_manager')->getZoneList($delivery_country);
+    if (!empty($zone_list)) {
+//      $form_values['delivery_address[zone]'] = array_rand($zone_list);
+    }
+
+    // Make the shipment.
+    $this->drupalPostForm(NULL, $form_values, t('Save shipment'));
+
+    // Check that we're now on the shipments overview page
+    $this->assertUrl('admin/store/orders/' . $order->id() . '/shipments');
+    $this->assertText(
+      'Shipment ID',
+      'Shipment summary found.'
+    );
+    $this->assertText(
+      '1234567890ABCD',
+      'Shipment data present.'
+    );
+
+    //
+    // Test "View", "Edit", "Print", "Packing slip" and "Delete" operations for this shipment.
+    //
+
+    // First, "View".
+    $this->drupalGet('admin/store/orders/' . $order->id() . '/shipments');
+    // (Use Href to distinguish View operation from View tab.)
+    $this->assertLinkByHref('admin/store/orders/' . $order->id() . '/shipments/1');
+    $this->drupalGet('admin/store/orders/' . $order->id() . '/shipments/1');
+    // Should find four tabs here:
+    $this->assertLinkByHref('admin/store/orders/' . $order->id() . '/shipments/1');
+    $this->assertLinkByHref('admin/store/orders/' . $order->id() . '/shipments/1/edit');
+    $this->assertLinkByHref('admin/store/orders/' . $order->id() . '/shipments/1/packing_slip');
+    $this->assertLinkByHref('admin/store/orders/' . $order->id() . '/shipments/1/print');
+    // We're editing the shipment we already made, so all the
+    // packages should be checked.
+//    foreach ($order->products as $sequence => $item) {
+//      $this->assertFieldByName(
+//        'products[' . $sequence . '][checked]',
+//        1,
+//        'Product is available for packaging.'
+//      );
+//    }
+
+    // Second, "Edit".
+    $this->drupalGet('admin/store/orders/' . $order->id() . '/shipments');
+    // (Use Href to distinguish Edit operation from Edit tab.)
+    $this->assertLinkByHref('admin/store/orders/' . $order->id() . '/shipments/1/edit');
+    $this->drupalGet('admin/store/orders/' . $order->id() . '/shipments/1/edit');
+    // We're editing the shipment we already made, so all the
+    // packages should be checked.
+//    foreach ($order->products as $sequence => $item) {
+//      $this->assertFieldByName(
+//        'products[' . $sequence . '][checked]',
+//        1,
+//        'Product is available for packaging.'
+//      );
+//    }
+
+    // Third "Print".
+    $this->drupalGet('admin/store/orders/' . $order->id() . '/shipments');
+    $this->assertLink(t('Print'));
+    $this->clickLink(t('Print'));
+    $this->assertUrl('admin/store/orders/' . $order->id() . '/shipments/1/print');
+//    foreach ($order->products as $sequence => $item) {
+//      $this->assertText(
+//        $item->qty->value . ' x ' . $item->model->value,
+//        'Product quantity x SKU found.'
+//      );
+//    }
+
+    // Fourth "Packing slip".
+    $this->drupalGet('admin/store/orders/' . $order->id() . '/shipments');
+    $this->assertLink(t('Packing slip'));
+    $this->clickLink(t('Packing slip'));
+    $this->assertUrl('admin/store/orders/' . $order->id() . '/shipments/1/packing_slip');
+    // Check for "Print invoice" and "Back" buttons.
+
+//    foreach ($order->products as $sequence => $item) {
+//      $this->assertText(
+//        $item->qty->value . ' x ' . $item->model->value,
+//        'Product quantity x SKU found.'
+//      );
+//    }
+
+    // Fifth, "Delete".
+    $this->drupalGet('admin/store/orders/' . $order->id() . '/shipments');
+    $this->assertLink(t('Delete'));
+    $this->clickLink(t('Delete'));
+    // Delete takes us to confirm page.
+    $this->assertUrl('admin/store/orders/' . $order->id() . '/shipments/1/delete');
+    $this->assertText(
+      'The shipment will be canceled and the packages it contains will be available for reshipment.',
+      'Deletion confirm question found.'
+    );
+    // "Cancel" returns to the shipment list page.
+    $this->clickLink(t('Cancel'));
+    $this->assertLinkByHref('admin/store/orders/' . $order->id() . '/shipments');
+
+    // Again with the "Delete".
+    $this->clickLink(t('Delete'));
+    $this->drupalPostForm(NULL, array(), t('Delete'));
+    // Delete returns to new packages page with all packages unchecked.
+    $this->assertUrl('admin/store/orders/' . $order->id() . '/shipments/new');
+    $this->assertText(
+      'Shipment 1 has been deleted.',
+      'Shipment deleted message found.'
+    );
+//    foreach ($order->products as $sequence => $item) {
+//      $this->assertFieldByName(
+//        'shipping_types[small_package][table][' . $sequence . '][checked]',
+//        0,
+//        'Package is available for shipping.'
+//      );
+//    }
 
   }
 
