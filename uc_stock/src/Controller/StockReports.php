9c615c88506fee51f9b833b0ef9c2d48e6a261dd
Tim Rohaly <tr@202830.no-reply.drupal.org>
2016-02-19 02:02:16 -0800

    Stock report: Replace Link:: with #type link, remove explicit <div>, use #type operations instead of text links. Fix link back to product page in report.

--- uc_stock/src/Controller/StockReports.php
+++ uc_stock/src/Controller/StockReports.php
@@ -8,7 +8,7 @@
 namespace Drupal\uc_stock\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Drupal\Core\Link;
+use Drupal\Core\Url;
 use Drupal\uc_report\Controller\Reports;
 
 /**
@@ -61,19 +61,27 @@ class StockReports extends ControllerBase {
 
     $result = $query->execute();
     foreach ($result as $stock) {
-      $op = array();
+      $op = '';
       if ($this->currentUser()->hasPermission('administer product stock')) {
-        $op[] = Link::createFromRoute($this->t('edit'), 'uc_stock.edit', ['node' => $stock->nid], ['query' => ['destination' => 'admin/store/reports/stock']])->toString();
+        $op = array(
+          '#type' => 'operations',
+          '#links' => array(
+            'edit' => array(
+              'title' => $this->t('Edit'),
+              'url' => Url::fromRoute('uc_stock.edit', ['node' => $stock->nid], ['query' => ['destination' => 'admin/store/reports/stock']]),
+            )
+          ),
+        );
       }
 
       // Add the data to a table row for display.
       $rows[] = array(
         'data' => array(
           array('data' => $stock->sku),
-          array('data' => Link::createFromRoute($stock->title, 'uc_stock.edit', ['node' => $stock->nid])->toString()),
+          array('data' => array('#type' => 'link', '#title' => $stock->title, '#url' => Url::fromRoute('entity.node.canonical', ['node' => $stock->nid]))),
           array('data' => $stock->stock),
           array('data' => $stock->threshold),
-          array('data' => implode(' ', $op)),
+          array('data' => $op),
         ),
         'class' => array(($stock->threshold >= $stock->stock) ? 'uc-stock-below-threshold' : 'uc-stock-above-threshold'),
       );
@@ -98,22 +106,28 @@ class StockReports extends ControllerBase {
     );
 
     $build['links'] = array(
-      '#prefix' => '<div class="uc-reports-links">',
-      '#suffix' => '</div>',
+      '#type' => 'container',
+      '#attributes' => array('class' => array('uc-reports-links')),
     );
     $build['links']['export_csv'] = array(
-      '#markup' => Link::createFromRoute($this->t('Export to CSV file'), 'uc_report.getcsv', ['report_id' => $csv_data['report'], 'user_id' => $csv_data['user']])->toString(),
+      '#type' => 'link',
+      '#title' => $this->t('Export to CSV file'),
+      '#url' => Url::fromRoute('uc_report.getcsv', ['report_id' => $csv_data['report'], 'user_id' => $csv_data['user']]),
       '#suffix' => '&nbsp;&nbsp;&nbsp;',
     );
 
 //    if (isset($_GET['nopage'])) {
 //      $build['links']['toggle_pager'] = array(
-//        '#markup' => Link::createFromRoute($this->t('Show paged records'), 'uc_stock.reports')->toString(),
+//        '#type' => 'link',
+//        '#title' => $this->t('Show paged records'),
+//        '#url' => Url::fromRoute('uc_stock.reports'),
 //      );
 //    }
 //    else {
       $build['links']['toggle_pager'] = array(
-        '#markup' => Link::createFromRoute($this->t('Show all records'), 'uc_stock.reports', [], ['query' => ['nopage' => '1']])->toString(),
+        '#type' => 'link',
+        '#title' => $this->t('Show all records'),
+        '#url' => Url::fromRoute('uc_stock.reports', [], ['query' => ['nopage' => '1']]),
       );
 //    }
 
