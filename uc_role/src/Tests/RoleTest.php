64dcc4582cafe1a5bde83da1dd06ccc015013be1
longwave <longwave@246492.no-reply.drupal.org>
2016-08-24 09:01:01 +0100

    Issue #2695639 by longwave: Zero price product generates error when checking out

--- uc_role/src/Tests/RoleTest.php
+++ uc_role/src/Tests/RoleTest.php
@@ -12,13 +12,16 @@ use Drupal\user\Entity\Role;
  */
 class RoleTest extends UbercartTestBase {
 
-  public static $modules = array('uc_payment', 'uc_payment_pack', 'uc_role');
+  // editor.module caused a crash on non-shippable products.
+  // @see https://www.drupal.org/node/2695639
+  public static $modules = array('uc_payment', 'uc_payment_pack', 'uc_role', 'editor');
 
   public function testRolePurchaseCheckout() {
-    // Add role assignment to the test product.
+    // Add role assignment to a free, non-shippable product.
+    $product = $this->createProduct(array('price' => 0, 'shippable' => 0));
     $rid = $this->drupalCreateRole(array('access content'));
     $this->drupalLogin($this->adminUser);
-    $this->drupalPostForm('node/' . $this->product->id() . '/edit/features', array('feature' => 'role'), t('Add'));
+    $this->drupalPostForm('node/' . $product->id() . '/edit/features', array('feature' => 'role'), t('Add'));
     $edit = array(
       'role' => $rid,
       'end_override' => TRUE,
@@ -28,10 +31,8 @@ class RoleTest extends UbercartTestBase {
     $this->drupalPostForm(NULL, $edit, t('Save feature'));
 
     // Check out with the test product.
-    $method = $this->createPaymentMethod('other');
-    $this->addToCart($this->product);
+    $this->addToCart($product);
     $order = $this->checkout();
-    uc_payment_enter($order->id(), $method['id'], $order->getTotal());
 
     // Test that the role was granted.
     // @todo Re-enable when Rules is available.
@@ -47,5 +48,18 @@ class RoleTest extends UbercartTestBase {
 
     // Run cron to ensure deleted users are handled correctly.
     $this->cronRun();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function populateCheckoutForm($edit = []) {
+    $edit = parent::populateCheckoutForm($edit);
+    foreach (array_keys($edit) as $key) {
+      if (substr($key, 0, 15) == 'panes[delivery]') {
+        unset($edit[$key]);
+      }
+    }
+    return $edit;
   }
 }
